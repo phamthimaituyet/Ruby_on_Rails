5ff09bad2324a75ddcdd8d3ce624f3674329a3eb
linhbtt85007 <linhbtt85007@co-well.vn>
2022-04-26 17:21:37 +0700

    add_function_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = Post.where(approve: current_user.id)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   devise_for :users
 
 
-  resources :users
+  resources :users 
 
   resources :posts do 
     resources :comments
