cf1d9f09fe091839adba44bdeb83e3170c112994
linhbtt85007 <linhbtt85007@co-well.vn>
2022-05-04 16:19:31 +0700

    reply

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   def new
     @post = Post.find(params[:post_id])
     @comment = Comment.new(parent_id: params[:parent_id])
-    
+
     respond_to do |format|
       format.html
       format.js
@@ -13,31 +13,30 @@ class CommentsController < ApplicationController
   
   def create
         @post = Post.find(params[:post_id])              # tìm bài viết đang đc comment 
-        @comment = @post.comments.create(comment_params)    # tạo comment cho bài viết đó 
-        if @comment.save
-          redirect_to post_path(@post)   
-        end                
-  end
-
-  def reply
-    @post = Post.find params[:post_id]
-    @comment = Comment.new
-    @parent = params[:id]
-    render :new
-  end
-
-      def destroy
-        @comment = @post.comments.find(params[:id])
-        if @comment.destroy
-          flash[:success] = 'Comment destroy!'
-          redirect_to post_path(@post)
-        else
-          flash[:danger] = 'Cannot destroy!'
-          redirect_to post_path(@post)
+        @comment = @post.comments.create(comment_params)    # tạo comment cho bài viết đó      
+        
+        respond_to do |format|
+          if @comment.save
+            format.html { redirect_to post_path(@post) }
+            format.js   { }
+          end
         end
-      end
-    private
-    def comment_params
-      params.require(:comment).permit(:commenter, :content, :parent_id)     # khởi tạo cmt 
+  end
+
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    if @comment.destroy
+      flash[:success] = 'Comment destroy!'
+      redirect_to post_path(@post)
+    else
+      flash[:danger] = 'Cannot destroy!'
+      redirect_to post_path(@post)
     end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :content, :parent_id)     # khởi tạo cmt 
+  end
 end
